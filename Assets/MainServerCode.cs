ec76f4cc05ceffe3c0d48593c67fafb3501fcc8b
NateAGeek <nate@nateageek.net>
2015-02-07 13:25:24 -0600

    Added basic Network Control should really be on a new branch

---
+++ Assets/MainServerCode.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainServerCode : MonoBehaviour {
+    
+    public string ipConnection = "127.0.0.1";
+    public int portConnection = 6969;
+    
+
+    void OnConnectedToServer() {
+        Network.Instantiate(Resources.Load("Prefabs/Player"), new Vector3(0, 0, 0), Quaternion.identity, 0);
+    }
+
+    void ConnectToServer() {
+        Network.Connect(ipConnection, portConnection);
+    }
+
+    void SetServer() {
+        Network.InitializeServer(4, portConnection, true);
+    }
+
+    void OnGUI() {
+        if (!Network.isClient && !Network.isServer) {
+            ipConnection = GUI.TextField(new Rect(10, 10, 100, 25), ipConnection);
+            int.TryParse(GUI.TextField(new Rect(10, 35, 100, 25), portConnection.ToString()), out portConnection);
+
+            if (GUI.Button(new Rect(10, 60, 200, 25), "Connect To Server (Client)")) {
+                ConnectToServer();
+            }
+            if (GUI.Button(new Rect(10, 100, 200, 25), "Become the Server (Host)")) {
+                SetServer();
+            }
+        }
+        else {
+            GUI.Label(new Rect(4, 24, 500, 16), "Connections: "+Network.connections.Length.ToString());
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Resources/Scripts/PlayerControl.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameObject cam;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+
+    public float speed = 10.0f;
+    public float gravity = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public bool canJump = true;
+    public float jumpHeight = 2.0f;
+    private bool grounded = false;
+    float rotationY = 0F;
+
+    void Start() {
+        if (networkView.isMine)
+        {
+            cam.camera.active = true;
+        }
+        else {
+            cam.camera.active = false;
+        }
+    }
+
+    void Update()
+    {
+        if (networkView.isMine)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+            if (Input.GetKeyDown("w"))
+            {
+                rigidbody.velocity = Vector3.forward;
+            }
+        }
+    }
+
+    void Awake()
+    {
+        rigidbody.freezeRotation = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (networkView.isMine)
+        {
+            Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity *= speed;
+
+            Vector3 velocity = rigidbody.velocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+            grounded = false;
+        }
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+}
