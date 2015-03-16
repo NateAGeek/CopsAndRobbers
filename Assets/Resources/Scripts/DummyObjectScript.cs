65b112ad764e140938f6a623c595598085b351ef
NateAGeek <nate@nateageek.net>
2015-03-16 15:31:40 -0500

    Map2.0 stuff

--- Assets/Resources/Scripts/DummyObjectScript.cs
+++ Assets/Resources/Scripts/DummyObjectScript.cs
@@ -8,7 +8,7 @@ public class DummyObjectScript : MonoBehaviour {
 	public Vector2 sensitivity  = new Vector2(10.0f, 10.0f);
 	public float   speed        = 1.0f;
 	public float   jumpVelocity = 5.0f;
-	private string selectedAbility = "StunTrap";
+	private string selectedAbility = "GrapHook";
 
 	//Component Vars
 	//	private Rigidbody rigidbody   = GetComponent<Rigidbody>();
