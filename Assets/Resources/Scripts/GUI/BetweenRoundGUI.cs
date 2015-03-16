169717e094fd19307a0c3e51ca5bf1f9399c8368
Kevin Ye <kevin.ye204@gmail.com>
2015-03-16 17:00:38 -0500

    Started abilities menu

--- Assets/Resources/Scripts/GUI/BetweenRoundGUI.cs
+++ Assets/Resources/Scripts/GUI/BetweenRoundGUI.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class BetweenRoundGUI : MonoBehaviour, IGUIState {
 	public RoundManager roundManager;
+	public Transform abilitiesMenu;
+
+	private string selectedAbility;
 
 	// Use this for initialization
 	void Start () {
-	
+		for(int i = 0; i < abilitiesMenu.childCount; i++){
+			Transform abSelect = abilitiesMenu.GetChild(i);
+			Button abBtn = abSelect.gameObject.GetComponent("Button") as Button;
+			Text abText = abSelect.Find("Text").gameObject.GetComponent("Text") as Text;
+			abBtn.onClick.AddListener(() => AbilityBtnClicked(abText.text));
+		}
 	}
 	
 	// Update is called once per frame
@@ -42,5 +51,20 @@ public class BetweenRoundGUI : MonoBehaviour, IGUIState {
 	public void ReadyBtnChecked(bool ready)
 	{
 		roundManager.SetReady(ready);
+	}
+
+	public void AbilityBtnClicked(string ability)
+	{
+		selectedAbility = ability;
+		for(int i = 0; i < abilitiesMenu.childCount; i++){
+			Transform abSelect = abilitiesMenu.GetChild(i);
+			Button abBtn = abSelect.gameObject.GetComponent("Button") as Button;
+			Text abText = abSelect.Find("Text").gameObject.GetComponent("Text") as Text;
+			if(abText.text == ability){
+				abBtn.interactable = false;
+			} else {
+				abBtn.interactable = true;
+			}
+		}
 	}
 }
